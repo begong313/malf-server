01e9f754844289393259fba677972093b693931e
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-07-15 21:25:55 +0900

    cors 추가

--- routes/picture.ts
+++ routes/picture.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from "express";
 import path from "path";
 import fs from "fs";
+import cors from "cors";
 
 const router: Router = Router();
+router.use(cors());
 
 router.get("/:picURL", (request: Request, response: Response) => {
     const imagePath = path.join(
