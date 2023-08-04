3c9e9378e689e306a42180222464a64d6bae09af
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-08-04 19:22:12 +0900

    코드 에러 수정

--- controllers/bulletinBoard.ts
+++ controllers/bulletinBoard.ts
@@ -89,12 +89,12 @@ function createPost(request: Request, response: Response) {
                         });
                         return;
                     }
-                    response.status(200).json({
-                        status: 200,
-                        post_id: results.insertId,
-                    });
                 }
             );
+            response.status(200).json({
+                status: 200,
+                post_id: results.insertId,
+            });
         }
     );
 }
