51ab96910bba24c52ba8088cef2f67b52a620b68
JongBin Noh <95959567+begong313@users.noreply.github.com>
2023-11-05 21:40:47 +0900

    .t1

--- passport/appleStrategy.ts
+++ passport/appleStrategy.ts
@@ -1,17 +1,26 @@
 import passport from "passport";
 import { Strategy } from "passport-apple";
 import { oauthConfig } from "../config/oauth_config";
+import jwt from "jsonwebtoken";
 import jwtGenerate from "../lib/jwtGenerator";
 import { getUserUniqId } from "./lib";
 
 function apple() {
     passport.use(
         new Strategy(
-            oauthConfig.apple,
+            {
+                clientID: process.env.APPLE_ID!,
+                teamID: process.env.APPLE_TEAM_ID!,
+                callbackURL: "/auth/apple/callback",
+                keyID: process.env.APPLE_KEY_ID!,
+                privateKeyString: process.env.APPLE_PRIVATE_KEY!,
+                passReqToCallback: true,
+            },
             async (req, accessToken, refreshToken, idToken, profile, cb) => {
-                console.log("apple idtoken", idToken);
+                const idTokenDecoded = jwt.decode(idToken);
+                console.log("apple idtoken", idTokenDecoded);
                 console.log("apple profile", profile);
-
+                //token변환시키고 값넣어주면될듯
                 const user_uniq_id: string = await getUserUniqId(
                     "apple",
                     profile.id
@@ -19,7 +28,7 @@ function apple() {
 
                 //jwt 생성
                 const jwtToken = jwtGenerate(user_uniq_id);
-                return cb(null, jwtToken);
+                cb(null, jwtToken);
             }
         )
     );
